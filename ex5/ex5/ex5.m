d5a02aff187ee5a56c4d6f0fdc1a7a9f23e72be3
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-08-19 22:21:27 -0400

    skip pauses in ex5

--- ex5/ex5/ex5.m
+++ ex5/ex5/ex5.m
@@ -57,8 +57,8 @@ J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+#fprintf('Program paused. Press enter to continue.\n');
+#pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
 %  You should now implement the gradient for regularized linear 
@@ -72,8 +72,8 @@ fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
          grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+#fprintf('Program paused. Press enter to continue.\n');
+#pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
