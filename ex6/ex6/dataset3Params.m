554cfb0e7860c9184d2c2968f5719529a3be1830
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-08-29 20:27:55 -0400

    solve the c and sigma optimization problem

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,43 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+CvaluesToTest = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmaValuesToTest = [0.003, 0.01 0.03 0.1 0.3 1 3 10 30];
+
+errorMatrix = zeros(length(CvaluesToTest), length(sigmaValuesToTest));
+
+for i = 1:length(CvaluesToTest)
+	for j = 1:length(sigmaValuesToTest)
+		curr_C = CvaluesToTest(i);
+		curr_sigma = sigmaValuesToTest(j);
+
+		model = svmTrain(X, y, curr_C, @(x1, x2) gaussianKernel(x1, x2, curr_sigma));
+
+		predictions = svmPredict(model, Xval);
+		errorMatrix(i,j) = mean(double(predictions ~= yval)); %~=, because == will calculate the accuracy.
+
+	end
+end
+
+%find the index of the lowest error in the matrix, that will give us desired C and sigma values
+
+[minError ind] = min(errorMatrix(:)); %convert matrix into vector, find the minimum error and it's index.
+
+[i j] = ind2sub(size(errorMatrix), ind); % find the subscript given a vector and matrix size.
 
 
+C = CvaluesToTest(i);
+sigma = sigmaValuesToTest(j);
 
-
+fprintf('C = %f and sigma = %f gives us error = %f', C, sigma, minError);
 
 
 % =========================================================================
