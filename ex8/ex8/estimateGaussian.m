3341f00f0be878135d7491e85d1d0848f1b60b8a
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-09-12 18:06:30 -0400

    finished estimateGaussian

--- ex8/ex8/estimateGaussian.m
+++ ex8/ex8/estimateGaussian.m
@@ -1,11 +1,11 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
@@ -21,13 +21,13 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
+% We entertain vectorized examples here. In reality, mu and sigma is implemented
+% by mean and var in octave
 
-
-
-
-
-
-
+sumX = sum(X)';
+mu = sumX / m;
+sumdiff = sum(bsxfun(@minus, X, mu').^2);
+sigma2 = sumdiff / m;
 
 
 % =============================================================
